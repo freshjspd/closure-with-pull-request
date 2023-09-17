a98d33544ec59f7c24827aeb2c399fde1b0d3da8
Viktoriia <pecourses>
2023-09-17 10:45:44 +0300

    Add babel to server

---
+++ build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var _process$env$PORT;
+var PORT = (_process$env$PORT = process.env.PORT) !== null && _process$env$PORT !== void 0 ? _process$env$PORT : 5000;
+console.log('PORT :>> ', PORT);
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const PORT = process.env.PORT ?? 5000;
+
+console.log('PORT :>> ', PORT);
